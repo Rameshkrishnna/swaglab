36bb0a0c66a3dfc9993c064ac6ced5aa30af745c
Rameshkrishnna <52850220+Rameshkrishnna@users.noreply.github.com>
2023-02-13 20:45:50 +0530

    13/02 commit

--- src/test/java/TestRunner/testRunner.java
+++ src/test/java/TestRunner/testRunner.java
@@ -1,5 +1,11 @@
 package TestRunner;
 
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(  
+	    features = "src/test/java/features",
+	    glue="loginPageSteps",monochrome=true,
+	    plugin= {"pretty","html:target/cucumber","json:target/cucumber.json"})
 public class testRunner {
 
 }
